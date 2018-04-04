76cd317e924714065fe1684dd3f30731cdfc0c3b
Olaf Krawczyk <radeko94@gmail.com>
2018-04-04 15:25:20 +0200

    register patient to doctor

---
+++ spring_backend/src/main/java/com/gubkra/infmed/infmedRest/service/doctor/DoctorService.java
@@ -0,0 +1,7 @@
+package com.gubkra.infmed.infmedRest.service.doctor;
+
+import java.util.UUID;
+
+public interface DoctorService {
+    void registerPatient(UUID doctor_uuid, UUID patient_UUID) throws IllegalArgumentException;
+}
---
+++ spring_backend/src/main/java/com/gubkra/infmed/infmedRest/service/doctor/DoctorServiceImpl.java
@@ -0,0 +1,51 @@
+package com.gubkra.infmed.infmedRest.service.doctor;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.gubkra.infmed.infmedRest.domain.AppUser;
+import com.gubkra.infmed.infmedRest.domain.Role;
+import com.gubkra.infmed.infmedRest.repository.RoleRepository;
+import com.gubkra.infmed.infmedRest.service.domain.user.UserService;
+import com.gubkra.infmed.infmedRest.utils.SecurityConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class DoctorServiceImpl implements DoctorService {
+    private static final Logger logger = LoggerFactory.getLogger(DoctorServiceImpl.class);
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Override
+    public void registerPatient(UUID doctor_uuid, UUID patient_uuid) throws IllegalArgumentException {
+        Optional<AppUser> doctor = userService.findById(doctor_uuid);
+        Optional<AppUser> patient = userService.findById(patient_uuid);
+
+        Role doctorRole = roleRepository.findByName(SecurityConstants.ROLE_DOCTOR);
+        Role patientRole = roleRepository.findByName(SecurityConstants.ROLE_PATIENT);
+
+        if (!doctor.isPresent() || !doctor.get().getRoles().contains(doctorRole)) {
+            throw new IllegalArgumentException(DoctorErrorMessages.INVALID_DOCTOR_ID);
+        }
+        if (!patient.isPresent() || !patient.get().getRoles().contains(patientRole)) {
+            throw new IllegalArgumentException(DoctorErrorMessages.INVALID_PATIENT_ID);
+        }
+
+        if (!doctor.get().getPatients().contains(patient.get())) {
+            doctor.ifPresent((d) -> d.getPatients().add(patient.get()));
+            userService.addItem(doctor.get());
+            logger.info("Registered patient [" + patient.get().getUsername() + "] to doctor ["
+                        + doctor.get().getUsername() + "]");
+        } else {
+            throw new IllegalArgumentException(DoctorErrorMessages.PATIENT_ALREADY_ASSIGNED);
+        }
+    }
+}
